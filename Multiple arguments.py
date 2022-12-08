2ae2c7d5e54bf33431bd2fd0d43a7b0c7af2fff5
Arthur Bernardes <arthur.feq.ufu@gmail.com>
2022-12-08 10:17:53 -0700

    Adding Multiple arguments

---
+++ Multiple arguments.py
@@ -0,0 +1,26 @@
+# Instructions
+# 100 XP
+# Use + to merge the contents of first and second into a new list: full.
+# Call sorted() on full and specify the reverse argument to be True. Save the sorted list as full_sorted.
+# Finish off by printing out full_sorted.
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse=True)
+a=sorted(second)
+
+# Print out full_sorted
+
+print(full_sorted)
+
+#Note how sorted with no reverse, sorts the numbers
+b=sorted(first)
+a=sorted(second)
+print(b)
+print(a)
